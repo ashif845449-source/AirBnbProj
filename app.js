73647c1f37b23880468b1387b08cea7d84c3366c
Ashif Ali <ashif845449@gmail.com>
2026-09-12 14:29:26 +0530

    Fix session serialization

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const store = MongoStore.create({
     mongoUrl: dbUrl,
     collectionName: "sessions_final",
+    stringify: false,
     touchAfter: 24 * 3600,
 });
 
