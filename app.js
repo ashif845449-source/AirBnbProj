fb4bf050a842f1707a62582668bea47efbcf258b
Ashif Ali <ashif845449@gmail.com>
2026-09-12 14:36:54 +0530

    fix session configuration

--- app.js
+++ app.js
@@ -66,6 +66,13 @@ app.use(flash());
 
 app.use(passport.initialize());
 app.use(passport.session());
+
+
+app.use((req, res, next) => {
+    res.locals.currUser = req.user;
+    next();
+});
+
 passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
