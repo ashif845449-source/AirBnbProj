2ed3e5d6738e6ac9e49682351c80bf974774954e
Ashif Ali <ashif845449@gmail.com>
2026-09-12 14:23:23 +0530

    Fix Mongo session store

--- app.js
+++ app.js
@@ -34,10 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
-    collectionName: "sessions_new",
-    crypto: {
-        secret: process.env.SESSION_SECRET,
-    },
+    collectionName: "sessions_final",
     touchAfter: 24 * 3600,
 });
 
